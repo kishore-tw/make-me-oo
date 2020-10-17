a074b36259bda4480dfedb099de0b4d1b9ef88bb
kishore <stkishore851999@gmail.com>
2020-10-17 18:32:11 +0530

    Refactor encapsulation design

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -2,14 +2,10 @@ package org.oop;
 
 public class DistanceAndDirectionCalculator {
     public static double distance(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+        return Math.sqrt(Math.pow(from.getXDistance(to), 2) + Math.pow(from.getYDistance(to), 2));
     }
 
     public static double direction(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.atan2(yDistance, xDistance);
+        return Math.atan2(to.getYDistance(from), to.getXDistance(from));
     }
 }
--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,17 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public double getXDistance(Point pointTwo) {
+        return this.x - pointTwo.x;
+    }
+
+    public double getYDistance(Point pointTwo) {
+        return this.y - pointTwo.y;
+    }
+
 }
